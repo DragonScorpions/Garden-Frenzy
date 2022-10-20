a1f8d9b22b7545d77f0567d31d61b954226745ec
Phil-Eggtree <icb0003@uah.edu>
2022-10-20 00:14:24 -0500

    Update Tile and Plot
    Updated the Tile class, which holds seeds, dictionary of seeds, and calculates when it should update its growth state. Tiles are also harvestable and return money based on dictionary. Tiles can also have seeds planted.

--- src/Main/PlayerData.java
+++ src/Main/PlayerData.java
@@ -10,11 +10,30 @@ package Main;
  */
 public class PlayerData {
     
-    public PlayerData(){
-            int money;
-            int plotsgrown;
-            
-        
+    private int money;
+    private int plants_grown;
+    private int plots_unlocked;
+   /**
+   * The default constructor of Playerdata.
+   * Sets all init values to zero
+   * @param money (int) How much money the player has.  
+   * @param plants_grown  (int) How many plants total has the player grown.
+   * @param plots_unlocked (int) How many plots has the player unlocked
+   */
+    public PlayerData(int money, int plants_grown, int plots_unlocked){
+            this.money = money;
+            this.plants_grown = plants_grown;
+            this.plots_unlocked = plots_unlocked;
     }
     
+    public int getMoney()
+    {
+        return money;
+    }
+    
+    public int getPlantsGrown()
+    {
+        return plants_grown;
+    }
+   
 }
--- src/Main/Tile.java
+++ src/Main/Tile.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Main;
+import java.util.Map;
 
 /**
  *
@@ -10,52 +11,137 @@ package Main;
  */
 public class Tile {
     
-    String currentSeed;
-    boolean isEnabled = false;
+    String currentSeed; //The current seed this tile holds
+    boolean enabled; //Can the user interact with this tile?
+    int growth_stage; //What state of growth is the seed in?
+    float time_since_growth; //When was the last time growth stage was updated?
+                      //In seconds from game start
+                      //(min=0=game start, max=game end time in seconds)
     
+    //The seed map, with each name of the seed stored with its value
+    Map<String, Integer> seed_worth = Map.of(
+    "None", 0,
+    "Pumpkin", 1,
+    "Wheat", 2,
+    "Corn", 3
+     );
     
+    //The seed map, with each name of the seed stored with how many stages it has
+    Map<String, Integer> seed_stages = Map.of(
+    "None", 999,
+    "Pumpkin", 2,
+    "Wheat", 2,
+    "Corn", 2
+     );
     
+    //The seed map, with each name of the seed stored with how long a stage is
+    //in seconds
+    Map<String, Integer> seed_time = Map.of(
+    "None", 999,
+    "Pumpkin", 10,
+    "Wheat", 10,
+    "Corn", 10
+     );
     
-    
-    
+    public Tile()
+    {
+        currentSeed = "None";
+        enabled = false;
+        growth_stage = 0;
+        time_since_growth = 0;
+    }
     
     public void disable(){
-        isEnabled = false;
+        enabled = false;
     }
     
-    public void enable(){
-
-        isEnabled = true;
+    public void enable()
+    {
+        enabled= true;
     }
-
     
+   /**
+   * Harvests the seed, giving money to player and resetting tile.
+   * Checks the seed against the seed dictionary, and gives money to the player
+   * based on the growth state. Then resets the tile.
+   * @return money How much money the seed was worth.
+   */
+    public int Harvest()
+    {
+        int worth;
+        //If growth state is equal to the ripe time, give player full money
+        if(growth_stage == seed_stages.get(currentSeed) )
+        {
+            worth = seed_worth.get(currentSeed); //Get the worth of the current seed
+        }
+        else //If it is not ripe yet, or has spoiled, return no money. 
+        {
+            worth = 0;
+        }
+        //TODO: Add sound effect to play? Or return sound effect?
+        currentSeed = " "; //Remove the seed
+        growth_stage = 0; //reset the growth stage
+        return worth; // return money gained - based on growth stage
+    }
     
-    // Only harvest if enabled
-    public int Harvest(){
-        currentSeed = " ";
+    /**
+   * Plants the given seed.
+   * Sets the tile's seed equal to the received seed.
+   * @param seed (string) What seed is being planted?
+   * @param time (float) How many seconds from game start did planting occur?
+   */
+    public void Plant(String seed, float time)
+    {
+        if(enabled)
+        {
+            currentSeed = seed;
+            time_since_growth = time;
+        }
+    }
+    
+    /**
+   * Updates the growth stage of the tile
+   * Advances tile's growth stage by one if it has not spoiled.
+   * 
+   */
+    private void updateGrowthState()
+    {
+        //Check if the current seed is ripe or is still growing
+        if(growth_stage <= seed_stages.get(currentSeed))
+            growth_stage++;
+        //Else, don't update. If this doesn't trigger,
+        //then the growth stage will be equal to one HIGHER than its ripe stage.
+        //This is intentional.
+        //TODO: Maybe return the state it is in and its associated image?
+    }
+    
+    /**
+   * Calculates if seed's stage should be increases.
+   * Takes the time and increases seed's growth state if enough time
+   * has passed.
+   * @param cur_time (float) How many seconds have passed since the game started
+   */
+    private void calcProgress(float cur_time)
+    {
+        //Calculate the difference between time since the last growth update  and the current time.
+        float time_difference = cur_time-time_since_growth;
         
-        return 0; // return money gained - based on dictionary - paceholder
+        //If the difference is equal to seed_time value, then update the current growth stage
+        //and reset time_since_growth.
+        if(seed_time.get(currentSeed) >= time_difference)
+        {
+            updateGrowthState();
+            time_since_growth = cur_time;
+        }
     }
     
-    
-    public void setCurrentSeed(String rcurrentSeed){
-        currentSeed = rcurrentSeed;
-    
-    }
-    
-    private void updateGrowthState(){
-        
-    }
-    
-    //Checks plant dictionary to see if amount of time has passed for seed to grow
-    //Calls updateGrowthState if it is time for seed to grown
-    private void calcProgress(){
-        
-        
-    }
-    
-    //update seedstate - calls calcProgress
-    public void Update(){
-        
+    /**
+   * Updates the tile
+   * Takes the time and increases seed's growth state if enough time
+   * has passed.
+   * @param cur_time (float) How many seconds have passed since the game started.
+   */
+    public void Update(float cur_time){
+        calcProgress(cur_time);
     }
 }
